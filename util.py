b14c500e4c198b800788620a0f2f12e5087ffc92
Haruka0522 <harukapc1@gmail.com>
2020-04-21 15:59:52 +0900

    クラスファイルを読み込む関数を追加

--- util.py
+++ util.py
@@ -8,6 +8,40 @@ import numpy as np
 import cv2
 
 
+def bbox_iou(box1, box2):
+    """
+    IoUの計算をする関数
+    """
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.max(b1_x2, b2_x2)
+    inter_rect_x2 = torch.max(b1_y2, b2_y2)
+
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * \
+        torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+
+    # Union Area
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x2 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+
+    return iou
+
+
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+
+    return tensor_res
+
+
 def predict_transform(
         prediction, inp_dim, anchors, num_classes, CUDA=True):
     """
@@ -59,3 +93,94 @@ def predict_transform(
     prediction[:, :, :4] *= stride
 
     return prediction
+
+
+def write_results(prediction, confidence, num_classes, nms_conf=0.4):
+    """
+    predict結果を受け取って、objectness scoreのしきい値処理とNon-maximal suppressionを行う
+    また、その結果を書き出す
+    confidenceはobjectness scoreのしきい値
+    nms_confはIoUのしきい値
+    """
+    conf_mask = (prediction[:, :, 4] > confidence).float().unsqueeze(2)
+    prediction = prediction * conf_mask
+
+    # IoUの計算
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
+    box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
+    box_corner[:, :, 1] = prediction[:, :, 1] - prediction[:, :, 3] / 2
+    box_corner[:, :, 2] = prediction[:, :, 0] + prediction[:, :, 2] / 2
+    box_corner[:, :, 3] = prediction[:, :, 1] + prediction[:, :, 3] / 2
+    prediction[:, :, :4] = box_corner[:, :, :4]
+
+    batch_size = prediction.size(0)
+
+    write = False  # 初期化していないフラグ
+
+    for idx in range(batch_size):
+        image_pred = prediction[idx]
+        max_conf, max_conf_score = torch.max(image_pred[:, 5:5+num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        seq = (image_pred[:, :5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+
+        non_zero_idx = torch.nonzero(image_pred[:, 4])
+        try:
+            image_pred_ = image_pred[non_zero_idx.squeeze(), :].view(-1, 7)
+        except:
+            continue
+        if image_pred_.shape[0] == 0:
+            continue
+
+        img_classes = unique(image_pred_[:, -1])  # 検出されたクラス
+
+        # NMSをクラスごとに実行
+        for cls in img_classes:
+            cls_mask = image_pred_ * \
+                (image_pred_[:, -1] == cls).float().unsqueeze(1)
+            class_mask_idx = torch.nonzero(cls_mask[:, -2]).squeeze()
+            image_pred_class = image_pred_[class_mask_idx].view(-1, 7)
+
+            # objectnessでsort
+            conf_sort_idx = torch.sort(
+                image_pred_class[:4], descending=True)[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            indx = image_pred_class.size(0)  # 検出されたnumber
+
+            # ここからがNMSの処理
+            for i in range(indx):
+                try:
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(
+                        0), image_pred_class[i+1:])
+                except ValueError:
+                    break
+                except IndexError:
+                    break
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i+1:] *= iou_mask
+
+                non_zero_idx = torch.nonzero(image_pred_class[:, 4]).squeeze()
+                image_pred_class = image_pred_class[non_zero_idx].view(-1, 7)
+
+            batch_idx = image_pred_class.new(
+                image_pred_class.size(0), 1).fill_(idx)
+            seq = batch_idx, image_pred_class
+
+            if not write:
+                output = torch.cat(seq, 1)
+                write = True
+            else:
+                out = torch.cat(seq, 1)
+                output = torch.cat((output, out))
+    try:
+        return output
+    except:
+        return 0
+
+
+def load_classes(namesfile):
+    fp = open(namesfile, "r")
+    names = fp.read().split("\n")[:-1]
+    return names
