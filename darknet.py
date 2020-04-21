836ebf2cf28800ca2c7cc5f12e120f89590bdf8c
Haruka0522 <harukapc1@gmail.com>
2020-04-21 14:54:59 +0900

    学習済みweightsファイルを読み込んでモデルに適用する処理を追加

--- darknet.py
+++ darknet.py
@@ -12,7 +12,7 @@ from util import *
 
 def get_test_input():
     img = cv2.imread("dog-cycle-car.png")
-    img = cv2.resize(img, (608, 608))
+    img = cv2.resize(img, (416, 416))
     img_ = img[:, :, ::-1].transpose((2, 0, 1))
     img_ = img_[np.newaxis, :, :, :] / 255.0
     img_ = torch.from_numpy(img_).float()
@@ -218,12 +218,85 @@ class Darknet(nn.Module):
 
         return detections
 
+    def load_weights(self, weight_file):
+        fp = open(weight_file, "rb")
+        header = np.fromfile(fp, dtype=np.int32, count=5)
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+        weights = np.fromfile(fp, dtype=np.float32)
 
-"""このコードが正常にかけているかのテスト
+        # weightsファイルをネットワークのモジュールに読み込む
+        ptr = 0
+        for i in range(len(self.module_list)):
+            module_type = self.blocks[i+1]["type"]
+
+            # 畳み込みのブロックの場合
+            if module_type == "convolutional":
+                model = self.module_list[i]
+                try:
+                    batch_normalize = int(self.blocks[i+1]["batch_normalize"])
+                except:
+                    batch_normalize = 0
+                conv = model[0]
+                if batch_normalize:
+                    bn = model[1]
+
+                    num_bn_biases = bn.bias.numel()
+
+                    bn_biases = torch.from_numpy(
+                        weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_weights = torch.from_numpy(
+                        weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_mean = torch.from_numpy(
+                        weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_var = torch.from_numpy(
+                        weights[ptr:ptr+num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_biases = bn_biases.view_as(bn.bias.data)
+                    bn_weights = bn_weights.view_as(bn.weight.data)
+                    bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                    bn_running_var = bn_running_var.view_as(bn.running_var)
+
+                    bn.bias.data.copy_(bn_biases)
+                    bn.weight.data.copy_(bn_weights)
+                    bn.running_mean.copy_(bn_running_mean)
+                    bn.running_var.copy_(bn_running_var)
+
+                else:
+                    num_biases = conv.bias.numel()
+
+                    conv_biases = torch.from_numpy(weights[ptr:ptr+num_biases])
+                    ptr += num_biases
+
+                    conv_biases = conv_biases.view_as(conv.bias.data)
+
+                    conv.bias.data.copy_(conv_biases)
+
+                num_weights = conv.weight.numel()
+                conv_weights = torch.from_numpy(weights[ptr:ptr+num_weights])
+                ptr += num_weights
+
+                conv_weights = conv_weights.view_as(conv.weight.data)
+                conv.weight.data.copy_(conv_weights)
+
+
+"""このコードが正常にかけているかのテスト"""
+"""
 #blocks = parse_cfg("cfg/yolov3.cfg")
 #print(create_modules(blocks))
+"""
 """
 model = Darknet("cfg/yolov3.cfg")
 inp = get_test_input()
 pred = model(inp, torch.cuda.is_available())
 print(pred)
+"""
+model = Darknet("cfg/yolov3.cfg")
+model.load_weights("yolov3.weights")
