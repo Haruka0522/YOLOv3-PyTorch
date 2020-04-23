be9d4ce516048b25e4cb6c49a1f24d79e12c2272
Haruka0522 <harukapc1@gmail.com>
2020-04-23 14:35:17 +0900

    なんか関数追加

--- util.py
+++ util.py
@@ -218,3 +218,11 @@ def load_classes(path):
     fp = open(path,"r")
     names = fp.read().split("\n")[:-1]
     return names
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
