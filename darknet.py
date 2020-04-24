822e0b1243184592cfd0176f353908af03db5362
Haruka0522 <harukapc1@gmail.com>
2020-04-24 10:39:21 +0900

    DetectionLayerの改修

--- darknet.py
+++ darknet.py
@@ -32,7 +32,7 @@ def create_modules(blocks):
     prev_filters = 3  # RGBで３層だから3を設定
     output_filters = []
 
-    for idx, x in enumerate(blocks[1:]):
+    for idx, x in enumerate(blocks[1:]):  # block[0]はハイパーパラメータなので除いている
         module = nn.Sequential()
 
         # 畳み込み層の場合
@@ -74,7 +74,7 @@ def create_modules(blocks):
         # upsampling層の場合
         elif (x["type"] == "upsample"):
             stride = int(x["stride"])
-            upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+            upsample = nn.Upsample(scale_factor=stride, mode="bilinear")
             module.add_module("upsample_{0}".format(idx), upsample)
 
         # route層の場合
@@ -113,8 +113,8 @@ def create_modules(blocks):
                        for i in range(0, len(anchors), 2)]  # 2つづつのタプルに区切る
             anchors = [anchors[i] for i in mask]
 
-            detection = DetectionLayer(anchors)
-            module.add_module("Detection_{0}".format(idx), detection)
+            detection = YOLOLayer(anchors)
+            module.add_module("yolo_{0}".format(idx), detection)
 
         module_list.append(module)
         prev_filters = filters
@@ -128,10 +128,142 @@ class EmptyLayer(nn.Module):
         super(EmptyLayer, self).__init__()
 
 
-class DetectionLayer(nn.Module):
-    def __init__(self, anchors):
-        super(DetectionLayer, self).__init__()
+class YOLOLayer(nn.Module):
+    def __init__(self, anchors, num_classes, img_dim=416):
+        super(YOLOLayer, self).__init__()
         self.anchors = anchors
+        self.num_anchors = len(anchors)
+        self.num_classes = num_classes
+        self.ignore_thres = 0.5
+        self.mse_loss = nn.MSELoss()  # loss関数のインスタンス
+        self.bce_loss = nn.BCELoss()  # loss関数のインスタンス
+        self.obj_scale = 1
+        self.noobj_scale = 100
+        self.metrics = {}
+        self.img_dim = img_dim
+        self.grid_size = 0  # grid size
+
+    def compute_grid_offsets(self, grid_size, cuda=True):
+        self.grid_size = grid_size
+        g = self.grid_size
+        FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+        self.stride = self.img_dim / self.grid_size
+
+        # 各gridのオフセットを計算
+        self.grid_x = torch.arange(g).repeat(
+            g, 1).view([1, 1, g, g]).type(FloatTensor)
+        self.grid_y = torch.arange(g).repeat(
+            g, 1).t().view([1, 1, g, g]).type(FloatTensor)
+        self.scaled_anchors = FloatTensor(
+            [(a_w / self.stride, a_h / self.stride) for a_w, a_h in self.anchors])
+        self.anchor_w = self.scaled_anchors[:, 0:1].view(
+            (1, self.num_anchors, 1, 1))
+        self.anchor_h = self.scaled_anchors[:, 1:2].view(
+            (1, self.num_anchors, 1, 1))
+
+    def forward(self, x, targets=None, img_dim=None):
+        # CUDA対応のTensorに上書きする
+        FloatTensor = torch.cuda.FloatTensor if x.is_cuda else torch.FloatTensor
+
+        self.img_dim = img_dim
+        num_samples = x.size(0)
+        grid_size = x.size(2)
+
+        prediction = (
+            x.view(num_samples, self.num_anchors,
+                   self.num_classes+5, grid_size, grid_size)
+            .permute(0, 1, 3, 4, 2)
+            .contiguous()
+        )
+
+        # 出力
+        print(prediction.shape)
+        x = torch.sigmoid(prediction[..., 0])  # center x
+        y = torch.sigmoid(prediction[..., 1])  # center y
+        w = prediction[..., 2]  # width
+        h = prediction[..., 3]  # height
+        pred_conf = torch.sigmoid(prediction[..., 0])  # conf
+        pred_cls = torch.sigmoid(prediction[..., 5])  # 予想されたclass
+
+        # grid_sizeと今が一致していない場合、新しくオフセットを計算する
+        if grid_size != self.grid_size:
+            self.compute_grid_offsets(grid_size, cuda=x.is_cuda)
+
+        # anchorsを使ってオフセットとスケールを追加する
+        pred_boxes = FloatTensor(prediction[..., :4].shape)
+        pred_boxes[..., 0] = x.data + self.grid_x  # x
+        pred_boxes[..., 1] = y.data + self.grid_y  # y
+        pred_boxes[..., 2] = torch.exp(w.data) * self.anchor_w  # w
+        pred_boxes[..., 3] = torch.exp(h.data) * self.anchor_h  # h
+
+        output = torch.cat(
+            (
+                pred_boxes.view(num_samples, -1, 4) * self.stride,
+                pred_conf.view(num_samples, -1, 1),
+                pred_cls.view(num_samples, -1, self.num_classes)
+            ),
+            -1
+        )
+
+        # targetsが指定されなかった場合
+        if targets is None:
+            return output, 0
+
+        else:
+            iou_scores, class_mask, obj_mask, noobj_mask, tx, ty, tw, th, tcls, tconf = build_targets(
+                pred_boxes=pred_boxes,
+                pred_cls=pred_cls,
+                target=targets,
+                anchors=self.scaled_anchors,
+                ignore_thres=self.ignore_thres,
+            )
+
+            # 存在しないオブジェクトを無視するようにする
+            loss_x = self.mse_loss(x[obj_mask], tx[obj_mask])
+            loss_y = self.mse_loss(y[obj_mask], ty[obj_mask])
+            loss_w = self.mse_loss(w[obj_mask], tw[obj_mask])
+            loss_h = self.mse_loss(h[obj_mask], th[obj_mask])
+            loss_conf_obj = self.bce_loss(pred_conf[obj_mask], tconf[obj_mask])
+            loss_conf_noobj = self.bce_loss(
+                pred_conf[noobj_mask], tconf[noobj_mask])
+            loss_conf = self.obj_scale * loss_conf_obj + self.noobj_scale * loss_conf_noobj
+            loss_cls = self.bce_loss(pred_cls[obj_mask], tcls[obj_mask])
+            total_loss = loss_x + loss_y + loss_w + loss_h + loss_conf + loss_cls
+
+            # Metricsの処理
+            cls_acc = 100 * class_mask[obj_mask].mean()
+            conf_obj = pred_conf[obj_mask].mean()
+            conf_noobj = pred_conf[noobj_mask].mean()
+            conf50 = (pred_conf > 0.5).float()
+            iou50 = (iou_scores > 0.5).float()
+            iou75 = (iou_scores > 0.75).float()
+            detected_mask = conf50 * class_mask * tconf
+            precision = torch.sum(iou50 * detected_mask) / \
+                (conf50.sum() + 1e-16)
+            recall50 = torch.sum(iou50 * detected_mask) / \
+                (obj_mask.sum() + 1e-16)
+            recall75 = torch.sum(iou75 * detected_mask) / \
+                (obj_mask.sum() + 1e-16)
+
+            # 計算した値をself.metricsにまとめる
+            self.metrics = {
+                "loss": to_cpu(total_loss).item(),
+                "x": to_cpu(loss_x).item(),
+                "y": to_cpu(loss_y).item(),
+                "w": to_cpu(loss_w).item(),
+                "h": to_cpu(loss_h).item(),
+                "conf": to_cpu(loss_conf).item(),
+                "cls": to_cpu(loss_cls).item(),
+                "cls_acc": to_cpu(cls_acc).item(),
+                "recall50": to_cpu(recall50).item(),
+                "recall75": to_cpu(recall75).item(),
+                "precision": to_cpu(precision).item(),
+                "conf_obj": to_cpu(conf_obj).item(),
+                "conf_noobj": to_cpu(conf_noobj).item(),
+                "grid_size": grid_size,
+            }
+
+            return output, total_loss
 
 
 class Darknet(nn.Module):
@@ -140,7 +272,10 @@ class Darknet(nn.Module):
         self.blocks = parse_model_config(cfg_file)
         self.net_info, self.module_list = create_modules(self.blocks)
 
-    def forward(self, x, CUDA):
+    def forward(self, x, targets=None, CUDA=None):
+        if CUDA == None:
+            CUDA = torch.cuda.is_available()
+        print(x.is_cuda)
         modules = self.blocks[1:]
         outputs = {}  # route層のキャッシュ用
 
