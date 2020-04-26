1e43adcae10d1f355302f16da7919c7cb4204e71
Haruka0522 <harukapc1@gmail.com>
2020-04-26 14:33:46 +0900

    Darknetクラスにsave_weights関数を追加

--- darknet.py
+++ darknet.py
@@ -373,6 +373,33 @@ class Darknet(nn.Module):
                 conv_weights = conv_weights.view_as(conv_layer.weight.data)
                 conv_layer.weight.data.copy_(conv_weights)
 
+    def save_weights(self,path,cutoff=-1):
+        """
+        weightsファイルを保存する
+        学習時に使う関数
+        """
+        with open(path,"wb") as w_file:
+            self.header_info[3] = self.seen
+            self.header_info.tofile(w_file)
+
+            for i,(module_def,module) in enumerate(zip(self.module_defs[:cutoff],self.module_list[:cutoff])):
+                if module_def["type"] == "convolutional":
+                    conv_layer = module[0]
+
+                    #batch normalizeが有効ならば
+                    if  module_def["batch_normalize"]:
+                        bn_layer = module[1]
+                        bn_layer.bias.data.cpu().numpy().tofile(w_file)
+                        bn_layer.weight.data.cpu().numpy().tofile(w_file)
+                        bn_layer.running_mean.data.cpu().numpy().tofile(w_file)
+                        bn_layer.running_var.data.cpu().numpy().tofile(w_file)
+
+                    else:
+                        conv_layer.bias.data.cpu().numpy().tofile(w_file)
+
+                    conv_layer.weight.data.cpu().numpy().tofile(w_file)
+
+
 
 """このコードが正常にかけているかのテスト"""
 """
