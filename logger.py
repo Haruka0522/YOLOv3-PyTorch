b03beed01073411f06421f31d4a96b4d65cc02da
Haruka0522 <harukapc1@gmail.com>
2020-04-27 21:01:51 +0900

    tensorflow==2.1.0に対応

--- logger.py
+++ logger.py
@@ -4,22 +4,23 @@ import tensorflow as tf
 class Logger(object):
     def __init__(self, log_dir):
         """logを記録するディレクトリを作成する"""
-        self.writer = tf.summary.FileWriter(log_dir)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
     def scale_summary(self, tag, value, step):
         """
         tag,value,stepをログに記録する。
         3つの変数はいずれもスカラー
         """
-        summary = tf.Summary(
-            value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step=step, description=None)
+            self.writer.flush()
 
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """
         tag_value_pairs,stepをログに記録する。
         3つの変数はいずれもスカラー
         """
-        summary = tf.Summary(value=[tf.Summary.Value(
-            tag=tag, simple_value=value) for tag, value in tag_value_pairs])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            for tag, value in tag_value_pairs:
+                tf.summary.scalar(tag, value, step=step, description=None)
+                self.writer.flush()
